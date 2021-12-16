0dfeddfaca2e2089d6b30d4253ba67abcc495339
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-12-15 21:03:22 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,91 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly OpeningTimeOption _option;
+
+        public ProjectsController(IOptions<OpeningTimeOption> option)
+        {
+            _option = option.Value;
+        }
+
+        //ex: api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            // listar todos os objetos
+            return Ok();
+        }
+
+        //ex: api/projects/1
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            // return NotFound();
+            // obter um objeto pelo id
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel projectModel)
+        {
+            if (projectModel.Title.Length > 50)
+                return BadRequest();
+
+            // cadastro o objeto
+            return CreatedAtAction(nameof(GetById), new { id = projectModel.Id }, projectModel);
+        }
+
+        //ex: api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel projectModel)
+        {
+            if (projectModel.Description.Length > 200)
+                return BadRequest();
+
+            // atualizo o objeto
+            return NoContent();
+        }
+
+        //ex: api/projects/3 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retorna NotFound
+
+            // Remover
+
+            return NoContent();
+        }
+
+        //ex: api/projects/1/comments POST
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment([FromBody] CreateCommentModel commentModel)
+        {
+            return NoContent();
+        }
+
+        //ex: api/projects/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        //ex: api/projects/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity() { } // Entity Framework
+
+        public int Id { get; private set; }
+
+    }
+}
